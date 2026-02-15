8cdc3d11d3874635c356795e32c93c8163e13af8
azure-553 <simmijin816@gmail.com>
2026-02-16 01:11:53 +0900

    memo: 파일명 대문자 => 소문자 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { Sidebar } from "@/components/Sidebar";
+import { Sidebar } from "@/components/sidebar";
 
 const pretendard = localFont({
 	src: "../public/fonts/PretendardVariable.ttf",
