568ad784fde7612e9cd3c8c2eea8884e300008b5
azure-553 <simmijin816@gmail.com>
2026-02-18 15:44:08 +0900

    feat: 새로 생성할 인사이트 레이아웃 추가

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,9 @@
-import { ComponentExample } from "@/components/component-example";
+import { InsightInput } from "@/components/insight-input";
 
 export default function DashboardPage() {
-	return <ComponentExample />;
+	return (
+		<div className="flex flex-col items-center justify-center min-h-screen gap-[40px] px-[240px]">
+			<InsightInput />
+		</div>
+	);
 }
