268b5fdb825e3eb6bb2b43cc7e63077bb4d5aec2
azure-553 <simmijin816@gmail.com>
2026-02-21 02:37:07 +0000

    refactor: remove saveAuthTokens function and update login success redirection to API endpoint

--- app/login-success/page.tsx
+++ app/login-success/page.tsx
@@ -1,5 +1,4 @@
 import { redirect } from "next/navigation";
-import { saveAuthTokens } from "./actions";
 import { LoginSuccessView } from "./login-success-view";
 
 interface PageProps {
@@ -10,8 +9,9 @@ export default async function LoginSuccessPage({ searchParams }: PageProps) {
 	const { accessToken, refreshToken } = await searchParams;
 
 	if (accessToken && refreshToken) {
-		await saveAuthTokens(accessToken, refreshToken);
-		redirect("/login-success");
+		redirect(
+			`/api/auth/token?accessToken=${accessToken}&refreshToken=${refreshToken}`,
+		);
 	}
 
 	return <LoginSuccessView />;
